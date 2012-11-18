563b17f81e1e745b1de5c640bb26becdaa1ce94f
Adwait Kulkarni <kulkarni.adwait@gmail.com>
2012-11-18 01:56:21 -0500

    Initial commit

---
+++ AstarPathFinding/AstarPathFinding/AstarPathFinding/Game1.cs
@@ -0,0 +1,358 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace AstarPathFinding
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        Vector2 position = Vector2.Zero;
+        //setting time greater because we want a path right from the start
+        double pathTimer = 6;
+        double pathTimeout = 3;
+        Vector2 direction = Vector2.Zero;
+        Texture2D movingPart;
+        Vector2 targetGridPosition;
+        bool chaseActive = false;
+
+        float speed = 100f;
+        Vector2 start, end;
+        int gridRows = 16;
+        int gridColumns = 26;
+        int GridSize = 50;
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        BasicEffect basicEffect;
+        VertexPositionColor[] rowVertices;
+        VertexPositionColor[] columnVertices;
+        PathFinder pathFinder;
+        Texture2D pathPoint;
+        bool[,] isWalkable;
+
+        Vector2 newPosition;
+        Vector2[] path;
+        Vector2[] oldPath;
+        int pathIndex = 0;
+        int oldPathIndex = 0;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            this.IsMouseVisible = true;
+            graphics.PreferredBackBufferWidth = gridColumns * GridSize;
+            graphics.PreferredBackBufferHeight = gridRows * GridSize;
+            graphics.ApplyChanges();
+            // TODO: Add your initialization logic here
+            basicEffect = new BasicEffect(graphics.GraphicsDevice);
+            basicEffect.VertexColorEnabled = true;
+            basicEffect.DiffuseColor = new Vector3(0, 0, 0.1f);
+
+            basicEffect.Projection = Matrix.CreateOrthographicOffCenter(0
+                , graphics.GraphicsDevice.Viewport.Width
+                , graphics.GraphicsDevice.Viewport.Height, 0, 0, 1);
+
+            rowVertices = new VertexPositionColor[2 * gridRows + 2];
+            int j = 0;
+            for (int i = 0; i <= gridRows; i++, j += 2)
+            {
+                rowVertices[j].Position = new Vector3(0, i * GridSize, 0);
+                rowVertices[j].Color = Color.Blue;
+                rowVertices[j + 1].Position = new Vector3(GridSize * gridColumns, i * GridSize, 0);
+                rowVertices[j + 1].Color = Color.Blue;
+            }
+
+            columnVertices = new VertexPositionColor[2 * gridColumns + 2];
+            j = 0;
+            for (int i = 0; i <= gridColumns; i++, j += 2)
+            {
+                columnVertices[j].Position = new Vector3(i * GridSize, 0, 0);
+                columnVertices[j].Color = Color.Blue;
+                columnVertices[j + 1].Position = new Vector3(i * GridSize, GridSize * gridRows, 0);
+                columnVertices[j + 1].Color = Color.Blue;
+            }
+
+            isWalkable = new bool[gridRows, gridColumns];
+            for (int i = 0; i < gridRows; i++)
+            {
+                for (j = 0; j < gridColumns; j++)
+                {
+                    isWalkable[i, j] = true;
+                }
+            }
+
+            //outer walls
+            for (int i = 0; i < gridRows; i++)
+            {
+                isWalkable[i, 0] = false;
+                isWalkable[i, gridColumns - 1] = false;
+            }
+            for (j = 0; j < gridColumns; j++)
+            {
+                isWalkable[0, j] = false;
+                isWalkable[gridRows - 1, j] = false;
+            }
+
+            start = new Vector2(1, 1);
+            end = new Vector2(gridColumns - 2, gridRows - 2);
+
+            position = start * GridSize;
+            targetGridPosition = end;
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            pathPoint = Content.Load<Texture2D>("pathPoint");
+            movingPart = Content.Load<Texture2D>("movingPart");
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        KeyboardState oldState = new KeyboardState();
+        KeyboardState newState;
+        MouseState oldMouse = new MouseState();
+        MouseState newMouse;
+        Vector2 newWall = Vector2.Zero;
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+            //mouse: add walls & start chase
+            MouseHandler();
+
+            //keyboard: move target
+            MoveTarget();
+
+            //Find path
+            pathTimer += gameTime.ElapsedGameTime.TotalSeconds;
+            //find new path after timeout
+            if (pathTimer > pathTimeout)
+            {
+                //if chaser is already on a paht then 
+                //find new path from current point in path
+                if (path != null && pathIndex < path.Length)
+                {
+                    start = path[pathIndex];
+                }
+                else//find path form current position
+                {
+                    start.X = (float)Math.Round(position.X / GridSize);
+                    start.Y = (float)Math.Round(position.Y / GridSize);
+                }
+                pathTimer = 0;
+                //save old path in case new target position is unreachable
+                if (path != null)
+                {
+                    oldPathIndex = pathIndex;
+                    oldPath = path;
+                }
+
+                //only find new path if target hasnt been reached
+                if (!start.Equals(targetGridPosition) && chaseActive)
+                {
+                    pathIndex = 0;
+                    pathFinder = new PathFinder(gridRows, gridColumns, isWalkable);
+                    path = pathFinder.findPath(start, targetGridPosition, isWalkable);
+
+                    //if new path path cant be foudn then take old path
+                    if (path == null)
+                    {
+                        path = oldPath;
+                        pathIndex = oldPathIndex;
+                    }
+                }
+            }
+            if (chaseActive)
+            {
+                Move();
+                Animate(gameTime);
+            }
+            base.Update(gameTime);
+        }
+
+        private void MoveTarget()
+        {
+            newState = Keyboard.GetState(PlayerIndex.One);
+            if (oldState != newState)
+            {
+                if (newState.IsKeyDown(Keys.Down))
+                {
+                    if (targetGridPosition.Y < gridRows - 2)
+                        targetGridPosition.Y += 1.0f;
+                }
+                if (newState.IsKeyDown(Keys.Up))
+                {
+                    if (targetGridPosition.Y > 1)
+                        targetGridPosition.Y -= 1.0f;
+                }
+                if (newState.IsKeyDown(Keys.Left))
+                {
+                    if (targetGridPosition.X > 1)
+                        targetGridPosition.X -= 1.0f;
+                }
+                if (newState.IsKeyDown(Keys.Right))
+                {
+                    if (targetGridPosition.X < gridColumns - 2)
+                        targetGridPosition.X += 1.0f;
+                }
+            }
+            oldState = newState;
+        }
+
+        private void MouseHandler()
+        {
+            newMouse = Mouse.GetState();
+            if (oldMouse != newMouse)
+            {
+                if (newMouse.LeftButton == ButtonState.Pressed)
+                {
+                    newWall.X = (int)(newMouse.X / GridSize);
+                    newWall.Y = (int)(newMouse.Y / GridSize);
+                    //cannot add or remove bounding walls
+                    if (newWall.X > 0 && newWall.X < gridColumns - 1)
+                    {
+                        if (newWall.Y > 0 && newWall.Y < gridRows - 1)
+                        {
+                            isWalkable[(int)newWall.Y, (int)newWall.X] = !isWalkable[(int)newWall.Y, (int)newWall.X];
+                        }
+                    }
+                }
+                else if (newMouse.RightButton == ButtonState.Pressed)
+                {
+                    chaseActive = !chaseActive;
+                }
+            }
+            oldMouse = newMouse;
+        }
+
+        private void Move()
+        {
+            if (path != null)
+            {
+                if (pathIndex < path.Length)
+                {
+                    direction = path[pathIndex] * GridSize - position;
+                    if (Math.Abs(direction.X) < 0.01f && Math.Abs(direction.Y) < 0.01f)
+                    {
+                        pathIndex++;
+                    }
+                    else
+                    {
+                        direction.Normalize();
+                    }
+                }
+                else
+                {
+                    direction = Vector2.Zero;
+                }
+            }
+        }
+
+        private void Animate(GameTime gameTime)
+        {
+            newPosition = position + direction * speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (isWalkable[(int)Math.Round(newPosition.Y / GridSize), (int)Math.Round(newPosition.X / GridSize)])
+            {
+                //position += direction * speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                position = newPosition;
+            }
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            Color color = Color.Blue;
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+            basicEffect.CurrentTechnique.Passes[0].Apply();
+            graphics.GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.LineList, rowVertices, 0, gridRows + 1);
+            graphics.GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.LineList, columnVertices, 0, gridColumns + 1);
+
+            spriteBatch.Begin();
+
+            for (int i = 0; i < gridRows; i++)
+            {
+                for (int j = 0; j < gridColumns; j++)
+                {
+                    if (isWalkable[i, j] == false)
+                    {
+                        spriteBatch.Draw(pathPoint, new Rectangle(j * (GridSize), i * (GridSize), GridSize, GridSize), Color.Blue);
+                    }
+                }
+
+            }
+
+            spriteBatch.Draw(movingPart, position, null, Color.White);
+            spriteBatch.Draw(movingPart, targetGridPosition * GridSize, null, Color.Purple);
+            spriteBatch.End();
+
+
+            spriteBatch.Begin();
+
+            if (path != null)
+            {
+                for (int i = 0; i < path.Length; i++)
+                {
+                    if (i == 0)
+                        color = Color.Red;
+                    else if (i == path.Length - 1)
+                        color = Color.Green;
+                    else
+                        color = Color.Yellow;
+
+                    spriteBatch.Draw(pathPoint, new Rectangle((int)path[i].X * (GridSize), (int)path[i].Y * (GridSize), 10, 10), color);
+                }
+            }
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+
+
+    }
+}
---
+++ AstarPathFinding/AstarPathFinding/AstarPathFinding/PathFinder.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace AstarPathFinding
+{
+    public class PathFinder
+    {
+        private const int MOVE_COST = 10;
+        private int gridRows;
+        private int gridColumns;
+
+        List<Vector2> openList = new List<Vector2>();
+        List<Vector2> closedList = new List<Vector2>();
+        Vector2[,] parentList;
+
+        bool[,] isWalkable;
+        int[,] F;
+        int[,] G;
+        int[,] H;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gridRows"></param>
+        /// <param name="gridColumns"></param>
+        /// <param name="isWalkable"></param>
+        public PathFinder(int gridRows, int gridColumns, bool[,] isWalkable)
+        {
+            this.gridRows = gridRows;
+            this.gridColumns = gridColumns;
+
+            init(isWalkable);
+           
+        }
+
+        private void init(bool[,] isWalkable)
+        {
+            this.isWalkable = isWalkable;   
+            parentList = new Vector2[gridRows, gridColumns];
+
+            F = new int[gridRows, gridColumns];
+            G = new int[gridRows, gridColumns];
+            H = new int[gridRows, gridColumns];
+            for (int i = 0; i < gridRows; i++)
+            {
+                for (int j = 0; j < gridColumns; j++)
+                {
+                    F[i, j] = int.MaxValue;
+                    G[i, j] = int.MaxValue;
+                    H[i, j] = int.MaxValue;
+                }
+            }
+        }
+
+        public Vector2[] findPath(Vector2 start, Vector2 goal, bool[,] isWalkable)
+        {
+            if (start.Equals(goal))
+            {
+                return null;
+            }
+            init(isWalkable);
+            Vector2 currVertex = start;
+            //put start node on frontier list
+            G[(int)start.Y, (int)start.X] = 0;
+            openList.Add(currVertex);
+
+            while (openList.Count > 0 && hasValue(closedList, goal) != true)
+            {
+                //select node with lowest F cost and repeat
+                currVertex = findLowestCostVertex();
+                //1. find all adjacent vertices
+                List<Vector2> adjList = findAdjacenyList(currVertex);
+                foreach (Vector2 adjVertex in adjList)
+                {
+                    //only do if the vertex is not already explored
+                    if (hasValue(closedList, adjVertex) != true)
+                    {
+                        //add each vertex to the open list
+                        if (hasValue(openList, adjVertex) != true)
+                        {
+                            openList.Add(adjVertex);
+                        }
+
+                        //2. calculate G cost to all adjacent vertices
+                        if (G[(int)adjVertex.Y, (int)adjVertex.X] >= G[(int)currVertex.Y, (int)currVertex.X] + MOVE_COST)
+                        {
+                            G[(int)adjVertex.Y, (int)adjVertex.X] = G[(int)currVertex.Y, (int)currVertex.X] + MOVE_COST;
+                            //3. calculate H cost for all adjacent vertices
+                            H[(int)adjVertex.Y, (int)adjVertex.X] = findHeuristicCost(adjVertex, goal);
+                            //4. calculate F cost for all adjacent vertices
+                            F[(int)adjVertex.Y, (int)adjVertex.X] = G[(int)adjVertex.Y, (int)adjVertex.X] + H[(int)adjVertex.Y, (int)adjVertex.X];
+                            //set current vertex as parent
+                            parentList[(int)adjVertex.Y, (int)adjVertex.X] = currVertex;
+                        }
+                    }
+                }
+
+                //put current vertex on explored list
+                removeValue(openList, currVertex);
+                closedList.Add(currVertex);
+            }
+
+
+            if (hasValue(closedList, goal) == true && hasValue(closedList, start) == true)
+            {
+                Vector2 pathVertex = goal;
+                List<Vector2> path = new List<Vector2>();
+                path.Add(pathVertex);
+                while (pathVertex != start)
+                {
+                    pathVertex = parentList[(int)pathVertex.Y, (int)pathVertex.X];
+                    path.Add(pathVertex);
+                }
+                path.Reverse();
+                return path.ToArray();
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Finds if there are walkable paths in the 4 directions around the current position.
+        /// Turning round a corner is not allowed diagonally
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        private List<Vector2> findAdjacenyList(Vector2 position)
+        {
+            List<Vector2> adjacencyList = new List<Vector2>();
+            int posX = (int)position.X;
+            int posY = (int)position.Y;
+            //check left
+            if (posX - 1 >= 0 && posY >= 0 && isWalkable[posY, posX - 1])
+            {
+                adjacencyList.Add(new Vector2(posX - 1, posY));
+            }
+            //check up
+            if (posY - 1 >= 0 && posX >= 0 && isWalkable[posY - 1, posX])
+            {
+                adjacencyList.Add(new Vector2(posX, posY - 1));
+            }
+            ////check left up
+            ////is left up legal
+            //if (posX - 1 > 0 && posY - 1 > 0 && isWalkable[posY - 1, posX - 1])
+            //{
+            //    //if up or left is blocked then this is blocked too
+            //    //no corner cutting
+            //    if (true)
+            //    {
+            //        adjacencyList.Add(new Vector2(posX - 1, posY - 1));
+            //    }
+            //}
+
+
+            //check right
+            if (posX + 1 < gridColumns && (posY >= 0 && posY < gridRows) && isWalkable[posY, posX + 1])
+            {
+                adjacencyList.Add(new Vector2(posX + 1, posY));
+            }
+            //check down
+            if (posY + 1 < gridRows && (posX >= 0 && posX < gridColumns) && isWalkable[posY + 1, posX])
+            {
+                adjacencyList.Add(new Vector2(posX, posY + 1));
+            }
+
+            return adjacencyList;
+        }
+
+        /// <summary>
+        /// Finds the heuristic(H) cost from start to goal.
+        /// Uses manhatten method
+        /// </summary>
+        /// <param name="current"></param>
+        /// <param name="goal"></param>
+        /// <returns></returns>
+        private int findHeuristicCost(Vector2 current, Vector2 goal)
+        {
+            int hCost = 0;
+            hCost = 5 * (int)(Math.Abs(goal.X - current.X) + Math.Abs(goal.Y - current.Y));
+            return hCost;
+        }
+
+        /// <summary>
+        /// Finds the vertex with the least F cost in the open list.
+        /// In case of a tie. The vertex added first wins.
+        /// </summary>
+        /// <returns></returns>
+        private Vector2 findLowestCostVertex()
+        {
+            int minCost = int.MaxValue;
+            Vector2 minVector = Vector2.Zero;
+            if (openList.Count == 1)
+                return openList[0];
+            foreach (Vector2 item in openList)
+            {
+                if (F[(int)item.Y, (int)item.X] <= minCost)
+                {
+                    minCost = F[(int)item.Y, (int)item.X];
+                    minVector = item;
+                }
+            }
+            return minVector;
+        }
+
+        private bool hasValue(List<Vector2> sourceList, Vector2 value)
+        {
+            foreach (Vector2 item in sourceList)
+            {
+                if (item.X == value.X && item.Y == value.Y)
+                    return true;
+            }
+            return false;
+        }
+
+        private void removeValue(List<Vector2> sourceList, Vector2 value)
+        {
+            if (hasValue(sourceList, value) == true)
+            {
+                int remIndex = -1;
+                for (int i = 0; i < sourceList.Count; i++)
+                {
+                    if (sourceList[i].X == value.X && sourceList[i].Y == value.Y)
+                    {
+                        remIndex = i;
+                        break;
+                    }
+                }
+                if (remIndex > -1)
+                {
+                    sourceList.RemoveAt(remIndex);
+                }
+            }
+        }
+    }
+}
